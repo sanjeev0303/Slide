75cab6b966540987873f19a377d26629cf3167c7
sanjeev0303 <sanjeev.081021@gmail.com>
2024-12-20 16:35:17 +0530

    adding automation dynamic page

--- src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
+++ src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
@@ -1,24 +1,31 @@
-import AutomationsBreadCrumb from '@/components/global/bread-crumbs/automation'
-import React from 'react'
+import AutomationsBreadCrumb from "@/components/global/bread-crumbs/automation";
+import { Warning } from "@/icons";
+import React from "react";
+import Trigger from "@/components/global/automations/trigger";
 
 type Props = {
-    params: {
-        id: string
-    }
-}
-
+  params: {
+    id: string;
+  };
+};
 
 // WIP: Set some metadata
 
-const Page = ({params}: Props) => {
-
-    //WIP: prefetch user automation data
+const Page = ({ params }: Props) => {
+  //WIP: prefetch user automation data
 
   return (
-    <div className='flex flex-col items-center gap-y-20'>
-        <AutomationsBreadCrumb id={params.id} />
+    <div className="flex flex-col items-center gap-y-20">
+      <AutomationsBreadCrumb id={params.id} />
+      <div className="w-full lg:w-10/12 xl:w-6/12 p-5 rounded-xl flex flex-col bg-[#1D1D1D] gap-y-3">
+        <div className="flex gap-x-2">
+            <Warning />
+            When...
+        </div>
+        <Trigger id={params.id} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
---
+++ src/components/global/automations/trigger/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type TriggerProps = {
+    id: string
+}
+
+const Trigger = ({ id }: TriggerProps) => {
+  return (
+    <div>Triggre</div>
+  )
+}
+
+export default Trigger
