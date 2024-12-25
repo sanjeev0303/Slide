5383e01eba145b43cb52805ede8879528b6468b9
sanjeev0303 <sanjeev.081021@gmail.com>
2024-12-25 10:59:11 +0530

    adding createUser

--- src/actions/user/index.ts
+++ src/actions/user/index.ts
@@ -2,8 +2,9 @@
 
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { findUser } from "./queries";
+import { createUser, findUser } from "./queries";
 import { refreshToken } from "@/lib/fetch";
+import { updateIntegration } from "../integration";
 
 export const onCurrentUser = async () => {
   const user = await currentUser();
@@ -35,9 +36,34 @@ export const onBoardUser = async () => {
           const today  = new Date()
           const expire_date = today.setDate(today.getDate() + 60)
 
-          const update_token = await
+          const update_token = await updateIntegration(
+            refresh.access_token,
+            new Date(expire_date),
+            found.integrations[0].id
+          )
+
+          if (!update_token) {
+            console.log("Update token failed");
+
+          }
+        }
+      }
+
+      return {
+        status: 200,
+        data: {
+            firstname: found.firstname,
+            lastname: found.lastname
         }
       }
     }
+
+    const created = await createUser(
+        user.id,
+        user.firstName!,
+        user.lastName!,
+        user.emailAddresses[0].emailAddress
+    )
+
   } catch (error) {}
 };
