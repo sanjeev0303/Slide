890418bb9d9786bb6088af511d738310bdf8b477
sanjeev0303 <sanjeev.081021@gmail.com>
2024-12-29 20:39:51 +0530

    adding trigger button

--- src/components/global/automations/tirgger-button/index.tsx
+++ src/components/global/automations/tirgger-button/index.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import PopOver from '../../pop-over'
+import React from "react";
+import PopOver from "../../pop-over";
+import { Divide } from "lucide-react";
+import { BlueAddIcon } from "@/icons";
 
 type TriggerButtonProps = {
-    children: React.ReactNode
-    label: string
-}
+  children: React.ReactNode;
+  label: string;
+};
 
-const TriggerButton = ({
-    label,
-    children
-}: TriggerButtonProps) => {
+const TriggerButton = ({ label, children }: TriggerButtonProps) => {
   return (
-    <PopOver />
-  )
-}
+    <PopOver
+      className="w-[400px]"
+      trigger={
+        <div className="border-2 border-dashed w-full border-[#3352CC] hover:opacity-80 cursor-pointer transition duration-100 rounded-xl flex gap-x-2 justify-center items-center p-5">
+            <BlueAddIcon />
+            <p className="text-[#768BDD] font-bold">{label}</p>
+        </div>
+      }
+    >
+        {children}
+    </PopOver>
+  );
+};
 
-export default TriggerButton
+export default TriggerButton;
