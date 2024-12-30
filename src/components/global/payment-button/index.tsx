831d0b1c3f79983f8eebe1c3700e3e9091e8a1c9
sanjeev0303 <sanjeev.081021@gmail.com>
2024-12-30 17:14:52 +0530

    addig use subscription

--- src/components/global/payment-button/index.tsx
+++ src/components/global/payment-button/index.tsx
@@ -1,10 +1,14 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
+import { useSubscription } from "@/hooks/subscription";
 import React from "react";
 
 type Props = {};
 
 const PaymentButton = (props: Props) => {
   // WIP: get their subscription details
+  const {} = useSubscription()
 
   return (
     <Button className="bg-gradient-to-br text-white rounded-full from-[#9685DB] via-[#9434E6] font-bold to-[#CC3BD4]">
